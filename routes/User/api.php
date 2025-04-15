1a2a0ce3d24d52bbc3d0046b79ca71777fc542c9
NJ-new <niravjaydev@gmail.com>
2025-04-15 19:21:23 +0530

    changes

--- routes/User/api.php
+++ routes/User/api.php
@@ -7,9 +7,9 @@ use App\Http\Controllers\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);#tested
 Route::post('/login', [AuthController::class, 'login']);#tested
-Route::post('/logout', [AuthController::class, 'logout']);#tested
 
 Route::group(['middleware'=>['auth:sanctum','user'], 'prefix' => 'user'],function()
 {
     Route::post('/volunteer-requests', [VolunteerRequestController::class, 'store']); #tested
+    Route::post('/logout', [AuthController::class, 'logout']);#tested
 });
