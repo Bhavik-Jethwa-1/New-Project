999cc523a904ff49263d3c0fbd98b7084a784fb1
NJ-new <niravjaydev@gmail.com>
2025-04-23 00:20:38 +0530

    changes made in config/cors.php

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie','*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:8080', 'http://192.168.218.158:8080',], // Add your network URL too
+    'allowed_origins' => ['http://localhost:5173'], // Add your network URL too
     'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Authorization', 'Accept', 'Origin'],
     'exposed_headers' => [],
     'max_age' => 0,
